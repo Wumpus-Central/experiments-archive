c2d891a1fa82a722723cee51651e17cb053ea529
DzikStar <dawid0mila@gmail.com>
2024-11-22 13:20:25 +0100

    experiment list item click event with openExperiment function

--- assets/app.js
+++ assets/app.js
@@ -8,12 +8,23 @@ function buildList(kind) {
 
     for (let i = 1; i <= 11; i++) {
         const li = document.createElement("li");
-        li.className = "experimentsListItem"
+        li.className = "experimentsListItem";
         li.textContent = `${kind}_${i}`;
+        li.addEventListener("click", function(){
+            openExperiment(li.textContent)
+        })
         ul.appendChild(li);
     }
 
     listContainer.appendChild(ul);
 
     pageMount.appendChild(listContainer);
+}
+
+function openExperiment(exp_id){
+    alert(exp_id)
+    /*
+        TODO
+    */
+   return;
 }
