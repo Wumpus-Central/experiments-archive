109ebca5793c48c35258b6d8378c61623569de4a
DzikStar <dawid0mila@gmail.com>
2024-11-22 12:53:40 +0100

    experiments list early implementation

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,19 @@
 function buildList(kind) {
-    const pageMount = document.getElementById('page-mount');
-    pageMount.innerHTML = `Placeholder - Type: ${kind}`;
+    const pageMount = document.getElementById("page-mount");
+
+    const listContainer = document.createElement("div");
+    listContainer.className = "experimentsListContainer";
+
+    const ul = document.createElement("ul");
+
+    for (let i = 1; i <= 7; i++) {
+        const li = document.createElement("li");
+        li.className = "experimentsListItem"
+        li.textContent = `Typ ${kind}, id ${i}`;
+        ul.appendChild(li);
+    }
+
+    listContainer.appendChild(ul);
+
+    pageMount.appendChild(listContainer);
 }
